787d572de6c961c40dd7c98aa2740383827d9734
Arol Gbeto-fia <g.senami94@gmail.com>
2020-11-03 01:38:38 -0500

    Commit 3.4

--- test.bat
+++ test.bat
@@ -1,7 +1,7 @@
-start "C:\Program Files\CodeBlocks\MinGW\bin"\g++.exe -Wall -fexceptions -g  -c calculator.cpp -o .\obj\Debug\calculator.o
+"C:\Program Files\CodeBlocks\MinGW\bin"\g++.exe -Wall -fexceptions -g  -c calculator.cpp -o .\obj\Debug\calculator.o
 
-start "C:\Program Files\CodeBlocks\MinGW\bin"\g++.exe  -o bin\Debug\UptimeFileProject.exe .\obj\Debug\calculator.o .\obj\Debug\main.o
+"C:\Program Files\CodeBlocks\MinGW\bin"\g++.exe  -o bin\Debug\UptimeFileProject.exe .\obj\Debug\calculator.o .\obj\Debug\main.o
 
-start bin\Debug\UptimeFileProject.exe
+bin\Debug\UptimeFileProject.exe
 
-start "C:\Program Files\Cppcheck"\cppcheck.exe --verbose --enable=all --enable=style --xml --output-file=cppCheckResults.xml .
+"C:\Program Files\Cppcheck"\cppcheck.exe --verbose --enable=all --enable=style --xml --output-file=cppCheckResults.xml .
